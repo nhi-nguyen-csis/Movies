f4da8fb7f4b925cd05f40b03aab1b2cc2e7017f6
nhi-nguyen-csis <nhi.nguyen.csis@gmail.com>
2022-03-04 12:45:13 -0800

    complete tab bar controller, network request, and collection view setup

---
+++ Movies/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Movies
+//
+//  Created by Nhi Nguyen on 3/4/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
---
+++ Movies/MovieGridViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MovieGridViewController.swift
+//  Movies
+//
+//  Created by Nhi Nguyen on 3/4/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    // create an array of dictionary
+    // type of key is String, type of value is Any
+    var movies = [[String:Any]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        //connect to the API network and get the list of similar superhero movie
+        let url = URL(string: "https://api.themoviedb.org/3/movie/634649/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                 // get the fetched dataDictionary from JSON and store only the result to movies
+                 // we need to cast the JSON format into an array of dictionary by using as!
+                 self.movies = dataDictionary["results"] as! [[String:Any]]
+                 
+                 // once we downloaded the complete all similar movies fetched data
+                 // we need to make tableview reload their data
+                 // reloadData() call the 2 func tableView again
+                 self.collectionView.reloadData()
+                 print(self.movies)
+             }
+        }
+        task.resume()
+    } // end viewDidLoad()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        let movie = movies[indexPath.item]
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        cell.posterView.af_setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+
+}
